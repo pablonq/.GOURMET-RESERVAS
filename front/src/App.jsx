557596fcc87e25e0209d0f15841c2d0b9cb28d2a
pablonq <pablo.navarro@est.fi.uncoma.edu.ar>
2024-09-10 11:20:19 -0300

    modificaciones

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-3xl font-bold underline'>HOLA</h1>
+     <h1 className='text-3xl font-bold underline text-teal-600'>.GOURMET-RESERVAS</h1>
     </>
   )
 }
