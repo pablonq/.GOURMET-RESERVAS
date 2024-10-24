10749753c0e3da62b0e61752ea21b6566629c707
PaulyPAREDES <pauly.pards@gmail.com>
2024-10-24 19:40:26 -0300

    Modificacion SoapController y platoControler porque antes no se cargaban las etiquetas, modificacion la pages CrearPlato del front, se hace un fetch ala ruta que va a traer las etiquetas del Soap, y la mapeamos en checkboxs para que al crear el plato pueda selecionar las tags

--- back/app/Http/Controllers/PlatoController.php
+++ back/app/Http/Controllers/PlatoController.php
@@ -12,12 +12,12 @@ class PlatoController extends Controller
 {
   public function indexPlatos($id)
   {
-   
+
     $restaurante = Restaurante::find($id);
 
     // Verificar si el restaurante existe
     if (!$restaurante) {
-        return response()->json(['error' => 'Restaurante no encontrado'], 404);
+      return response()->json(['error' => 'Restaurante no encontrado'], 404);
     }
 
     // Cargar los platos del restaurante con los menús asociados
@@ -25,28 +25,28 @@ class PlatoController extends Controller
 
     // Modificar la estructura de la respuesta para incluir los menús asociados
     $platosConMenus = $platos->map(function ($plato) {
-        return [
-            'id' => $plato->id,
-            'nombrePlato' => $plato->nombrePlato,
-            'descripcion' => $plato->descripcion,
-            'informacionNutricional' => $plato->informacionNutricional,
-            'precio' => $plato->precio,
-            'categoria' => $plato->categoria,
-            'imagen' => $plato->imagen,
-            // Devolver una lista de los menús asociados con el plato
-            'menus' => $plato->menus->map(function ($menu) {
-                return [
-                    'id' => $menu->id,
-                    'nombre' => $menu->nombre,
-                ];
-            }),
-        ];
+      return [
+        'id' => $plato->id,
+        'nombrePlato' => $plato->nombrePlato,
+        'descripcion' => $plato->descripcion,
+        'informacionNutricional' => $plato->informacionNutricional,
+        'precio' => $plato->precio,
+        'categoria' => $plato->categoria,
+        'imagen' => $plato->imagen,
+        // Devolver una lista de los menús asociados con el plato
+        'menus' => $plato->menus->map(function ($menu) {
+          return [
+            'id' => $menu->id,
+            'nombre' => $menu->nombre,
+          ];
+        }),
+      ];
     });
 
     // Devolver la respuesta en formato JSON
     return response()->json($platosConMenus);
-             
   }
+
   public function crearPlato(Request $request)
   {
     $fields = $request->validate([
@@ -56,7 +56,9 @@ class PlatoController extends Controller
       'informacionNutricional' => 'required|string',
       'precio' => 'required|integer',
       'categoria' => 'required|string',
-    
+      'tags' => 'array',
+      'tags.*' => 'string',
+
       'imagen' => 'required',
       'idMenu' => 'nullable|integer',  // El idMenu puede ser null o un entero
     ]);
@@ -69,52 +71,57 @@ class PlatoController extends Controller
       'informacionNutricional' => $fields['informacionNutricional'],
       'precio' => $fields['precio'],
       'categoria' => $fields['categoria'],
-      'tags' => null,
       'imagen' => $fields['imagen'],
-      
+      'tag' => json_encode($fields['tags'])
+
     ]);
     $restaurante->platos()->attach($plato->id);
+
+
+
     if (!is_null($fields['idMenu'])) {
       // Buscar el menú
       $menu = Menu::find($fields['idMenu']);
-      
+
       // Si el menú existe, crear la relación en la tabla pivote plato_menu
       if ($menu) {
-          $menu->platos()->attach($plato->id);
+        $menu->platos()->attach($plato->id);
       }
-  }
+    }
 
     return response()->json($plato);
   }
+
   public function getPlato($id)
   {
-    
+
     $plato = Plato::with('menus')->find($id);
 
     if (!$plato) {
-        return response()->json(['error' => 'Plato no encontrado'], 404);
+      return response()->json(['error' => 'Plato no encontrado'], 404);
     }
 
-    
+
     $menu = $plato->menus->isNotEmpty() ? $plato->menus->first() : null;
 
-    
+
     return response()->json([
-        'id' => $plato->id,
-        'nombrePlato' => $plato->nombrePlato,
-        'descripcion' => $plato->descripcion,
-        'informacionNutricional' => $plato->informacionNutricional,
-        'precio' => $plato->precio,
-        'categoria' => $plato->categoria,
-        'imagen' => $plato->imagen,
-        'menu' => $menu ? [
-            'id' => $menu->id,
-            'nombre' => $menu->nombre,
-        ] : null,  
+      'id' => $plato->id,
+      'nombrePlato' => $plato->nombrePlato,
+      'descripcion' => $plato->descripcion,
+      'informacionNutricional' => $plato->informacionNutricional,
+      'precio' => $plato->precio,
+      'categoria' => $plato->categoria,
+      'imagen' => $plato->imagen,
+      'menu' => $menu ? [
+        'id' => $menu->id,
+        'nombre' => $menu->nombre,
+      ] : null,
     ]);
   }
 
-  public function editarPlato(Request $request, $id){
+  public function editarPlato(Request $request, $id)
+  {
     $fields = $request->validate([
       'idRestaurante' => 'required|integer',
       'nombrePlato' => 'required|string',
@@ -124,28 +131,28 @@ class PlatoController extends Controller
       'categoria' => 'required|string',
       'tags' => 'nullable',
       'idMenu' => 'nullable|integer',
-    
+
       /* 'imagen' => 'required', */
     ]);
     $platoData = $request->only(['nombrePlato', 'descripcion', 'informacionNutricional', 'precio', 'categoria']);
     $plato = Plato::find($id);
     if (!$plato) {
       return response()->json(['error' => 'Plato no encontrado'], 404);
-  }
-    
-      
-        $plato->update($platoData);
+    }
 
-        if (empty($fields['idMenu'])) {
-          $plato->menus()->detach();  // Eliminar todos los menús asociados al plato
-      } else {
-          // Si se seleccionó un menú, actualizar la relación
-          $plato->menus()->sync([$fields['idMenu']]);
-      }
-      
 
-      
-        return response()->json($plato);
+    $plato->update($platoData);
+
+    if (empty($fields['idMenu'])) {
+      $plato->menus()->detach();  // Eliminar todos los menús asociados al plato
+    } else {
+      // Si se seleccionó un menú, actualizar la relación
+      $plato->menus()->sync([$fields['idMenu']]);
+    }
+
+
+
+    return response()->json($plato);
   }
   public function borrarPlato($id)
   {
@@ -153,16 +160,16 @@ class PlatoController extends Controller
     $plato->restaurantes()->detach();
     $plato->delete();
     return ['message' => 'Plato Eliminado'];
-}
+  }
 
-public function indexPlatosMenus($menuId)
-{
+  public function indexPlatosMenus($menuId)
+  {
     // Buscar el menú por su ID
     $menu = Menu::find($menuId);
 
     // Verificar si el menú existe
     if (!$menu) {
-        return response()->json(['error' => 'Menú no encontrado'], 404);
+      return response()->json(['error' => 'Menú no encontrado'], 404);
     }
 
     // Obtener los platos asociados al menú
@@ -170,5 +177,5 @@ public function indexPlatosMenus($menuId)
 
     // Devolver los platos en formato JSON
     return response()->json($platos);
-}
+  }
 }
--- back/app/Http/Controllers/SoapController.php
+++ back/app/Http/Controllers/SoapController.php
@@ -3,21 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use SoapClient;
+use SoapFault;
 
 class SoapController extends Controller
 {
     public function getDishTags()
     {
-        $wsdl = 'http://localhost/soap-service/index.php?wsdl';
 
         try {
-            $client = new \SoapClient($wsdl);
+            $client = new SoapClient(
+                null,
+                array(
+                    'location' => 'http://localhost/soap-service/index.php',
+                    'uri' => 'http://localhost/soap-service',
+                    'trace' => 1,
+                )
+            );
             $tags = $client->getDishTags();
+
             return response()->json($tags);
-        } catch (\SoapFault $fault) {
-            return response()->json(['error' => $fault->getMessage()], 500);
+        } catch (SoapFault $fault) {
+            Log::error('SOAP Error: ' . $fault->getMessage());
+            return response()->json(['error' => 'Error al obtener las etiquetas.'], 500);
+        } catch (Exception $e) {
+            Log::error('Error: ' . $e->getMessage());
+            return response()->json(['error' => 'Error inesperado.'], 500);
         }
     }
-    
 }
