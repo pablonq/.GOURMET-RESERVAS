8b137169aaebe874e462c0cee7a39c6669040366
pablonq <pablo.navarro@est.fi.uncoma.edu.ar>
2024-11-01 10:40:19 -0300

    laravel.log

--- back/routes/console.php
+++ back/routes/console.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// routes/console.php
+
+Artisan::command('logs:clear', function () {
+  file_put_contents(storage_path('logs/laravel.log'), '');
+  $this->info('Logs limpiados correctamente.');
+})->describe('Limpiar el archivo laravel.log');
